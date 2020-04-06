32b81861d0d536edb2377a7fafe38c07d9bcb110
XHY <hello@ioslide.com>
2020-04-06 12:57:26 +0800

    moon/margin-10

--- miniprogram/weatherui/config/config.js
+++ miniprogram/weatherui/config/config.js
@@ -3,16 +3,14 @@ Object.defineProperty(exports, "__esModule", {
   value: !0
 });
 
-var e = [ "theme", "darkMode", "location", "history", "storageVersion" ], t = [ "addminapp", "menustatus", "storage_version", "guide_version", "setting", "locals", "searchData", "textColorMode", "textWeightMode" ];
-
 exports.default = {
   caiyunApiHost: "https://api.caiyunapp.com/",
   caiyunApiVersion: "v2.5/",
   caiyunApiToken: "yJgJQ31wdVX4RDrE/",
+  // F4i9DpgD0R1DIcPP
   qqMapToken: "AVPBZ-T2ERX-CAK4M-77647-JJQHQ-VKFD4",
+  // 47ABZ-AJN3P-POPDO-VGI22-X5PBV-ZTFFP
   storageVersion: "trial1",
-  storageKey: e,
-  removeStorageKey: t,
   name: "XHY",
   description: "",
   date: "",
